7a2f940793c794a9b79e0f3ff1b0eb5942eb1186
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-07-15 08:28:16 +0530

    Add: Create a BST by adding 56 and then adding 30 & 70

---
+++ BinarySearchTree.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.bridgelabz.main;
+
+/**
+ * @author MD_AFROZ
+ *
+ */
+public class BinarySearchTree<K extends Comparable<K>> {
+
+	public Node<K> root;
+
+	public void add(K key) {
+		this.root = this.addRecursively(root, key);
+
+	}
+
+	public Node<K> addRecursively(Node<K> current, K key) {
+		if (current == null)
+			return new Node<>(key);
+		int compareResult = key.compareTo(current.key);
+		if (compareResult == 0)
+			return current;
+		else if (compareResult < 0)
+			current.leftNode = addRecursively(current.leftNode, key);
+		else {
+			current.rightNode = addRecursively(current.rightNode, key);
+		}
+		return current;
+	}
+
+}
